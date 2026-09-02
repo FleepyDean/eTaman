38cb302b1215b097b96f37e5cf28c64a51b50a3d
Danish <danishflip47@gmail.com>
2026-09-02 23:46:28 +0800

    Configure nginx to use dynamic PORT from environment variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# Substitute BACKEND_URL env var into nginx config at runtime
-# Default to backend's public Railway domain
+# Substitute BACKEND_URL and PORT env vars into nginx config at runtime
 BACKEND_URL="${BACKEND_URL:-http://backend.railway.internal:8000}"
+PORT="${PORT:-80}"
 sed -i "s|\${BACKEND_URL}|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf
+sed -i "s|\${PORT}|${PORT}|g" /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
